79a21dabf54ee5b41a88bc1de11b1a50f389b406
YESHWANTH R <yeshuyeshwanth2005@gmail.com>
2024-07-17 23:15:49 +0530

    Products list display

--- ecommerce-admin-panel/app/products/page.js
+++ ecommerce-admin-panel/app/products/page.js
@@ -1,24 +1,75 @@
+"use client";
 import LayoutMain from "@/components/MainLayout";
-import connectDB from "@/lib/connectDB";
+import "@/public/Stylesheets/scrollBar.css";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const [products, setProducts] = useState([]);
   const fecthData = async () => {
-    let res = await fetch("");
+    let res = await fetch("http://localhost:3000/api/getProducts");
+    let data = await res.json();
+    setProducts(data);
+    console.log(data);
   };
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
   return (
     <div>
       <LayoutMain>
-        <div className="flex gap-10 flex-col">
-          <label htmlFor="">To add new Product</label>
-          <div>
-            <Link
-              href={"/products/new"}
-              className="bg-red-500 text-white hover:bg-red-600 text-xl px-5 py-3 rounded-xl"
-            >
-              Add
-            </Link>
+        <div className="flex gap-5 flex-col h-full overflow-hidden bg-red-50">
+          <div className="bg-white m-3 overflow-y-scroll h-full rounded-xl shadow-2xl">
+            <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
+              <div className="flex w-full justify-between items-center">
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                  Products
+                </h2>
+                <Link
+                  href="/products/new"
+                  className="bg-red-500 text-white hover:bg-red-600 text-xl px-5 py-3 rounded-xl"
+                >
+                  Add Products
+                </Link>
+              </div>
+              <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-10">
+                {products.map((product) => (
+                  <div
+                    key={product._id}
+                    className="group relative shadow-lg p-2 rounded-lg border"
+                  >
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      {/* <img
+              alt={product.imageAlt}
+              src={product.imageSrc}
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            /> */}
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <Link href={"/api/editProducts/" + product._id}>
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            />
+                            {product.Name}
+                          </Link>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">
+                          {product.description}
+                        </p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">
+                        ₹{product.Price}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </LayoutMain>
--- ecommerce-admin-panel/components/NavBar.js
+++ ecommerce-admin-panel/components/NavBar.js
@@ -1,23 +1,39 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import nameLogo from "@/public/images/nameLogo.png";
+import React from 'react'
+import { useSession } from "next-auth/react";
+import { FaBell, FaUser } from "react-icons/fa";
+import { HiOutlineBell } from "react-icons/hi";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const NavBar = () => {
+  const { data: session, status } = useSession();
+
   return (
-    <div className="flex mb-1 border-b-2">
-      <Link
-        href="/"
-        className="flex justify-center mt-1 items-center text-gray-900 rounded-lg rounded-r-none"
-      >
-        <Image
-          width={200}
-          src={nameLogo}
-          className="rounded-xl mx-5 p-1 hover:bg-red-300"
-        ></Image>
-      </Link>
-    </div>
+    <div className="flex flex-col h-20 border shadow-red-500">
+        <div className="py-2 flex w-full justify-between px-5 text-lg items-center">
+          <span>Welcome {session?.user.name}</span>
+          <div className='flex w-1/4 gap-1'>
+            <div className='flex flex-col justify-center items-center'>
+              <Link href={"/inbox"} className='bg-red-50 border cursor-pointer p-3 flex justify-center items-center rounded-full'>
+
+              <HiOutlineBell  className=''/>
+              </Link>
+            </div>
+            <div className="flex gap-2 justify-center items-center py-1 bg-red-500 rounded-full px-2">
+              <span className="text-white">{session?.user.name}</span>
+              <div className="rounded-full overflow-hidden">
+                {session ? (
+                  <Image src={session?.user.image} width={40} height={40} />
+                ) : (
+                  <FaUser />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
   );
 };
 
-export default NavBar;
+export default NavBar
