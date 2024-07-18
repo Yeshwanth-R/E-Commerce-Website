a859769e0e22ab430a9518409bfc6f63871f5e5b
YESHWANTH R <yeshuyeshwanth2005@gmail.com>
2024-07-18 12:44:37 +0530

    update

--- ecommerce-admin-panel/app/products/editProducts/[...id]/page.js
+++ ecommerce-admin-panel/app/products/editProducts/[...id]/page.js
@@ -135,7 +135,7 @@ const editPro = () => {
                         <div className="">
                             <button
                                 type="submit"
-                                class="text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
+                                className="text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
                             >
                                 Save
                             </button>
