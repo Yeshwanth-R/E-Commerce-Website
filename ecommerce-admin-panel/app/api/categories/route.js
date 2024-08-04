4ff71001cbcd71466fb833758a590f15d4fce316
YESHWANTH R <eng23cs0234@dsu.edu.in>
2024-08-04 11:29:45 +0530

    Delete button of edit going on

--- ecommerce-admin-panel/app/api/categories/route.js
+++ ecommerce-admin-panel/app/api/categories/route.js
@@ -7,10 +7,23 @@ export async function POST(req) {
         await connectDB();
 
         const data = await req.json();
-        console.log("Received data:", data.name); // Logging received data
-
+        console.log("Received data:", data.name);
         const category = await Category.create(data);
-        console.log("Category created:", category); // Logging created product
+        console.log("Category created:", category);
+
+        return NextResponse.json({ message: "Category Created successfully" });
+    } catch (error) {
+        console.error("Error creating category:", error); // Logging error
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
+export async function PUT(req) {
+    try {
+        await connectDB();
+        const data = await req.json();
+        console.log("Received data:", data);
+        const category = await Category.findByIdAndUpdate(data.id, data)
+        console.log("Category Updated:", category);
 
         return NextResponse.json({ message: "Category Created successfully" });
     } catch (error) {
@@ -27,6 +40,24 @@ export async function GET(req, res) {
         return NextResponse.json(categories);
     } catch (error) {
         console.error("Error:", error); // Logging error
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
+
+
+
+export async function DELETE(req) {
+    try {
+        await connectDB();
+        const data = await req.json();
+        console.log(data)
+        console.log("Received data:", data);
+        const category = await Category.findByIdAndDelete(data)
+        console.log("Category Deleted:", category);
+
+        return NextResponse.json({ message: "Category Created successfully" });
+    } catch (error) {
+        console.error("Error creating category:", error); // Logging error
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
