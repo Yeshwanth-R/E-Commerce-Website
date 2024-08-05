be7bf8770070f18050f1726011b4f6e63c4fb931
YESHWANTH R <yeshuyeshwanth2005@gmail.com>
2024-08-05 22:40:27 +0530

    Back Button

--- ecommerce-admin-panel/app/products/editProducts/[...id]/page.js
+++ ecommerce-admin-panel/app/products/editProducts/[...id]/page.js
@@ -1,6 +1,9 @@
+"use client";
 import React from "react";
 import LayoutMain from "@/components/MainLayout";
-import { toast, Toaster } from "sonner";
+import { HiChevronLeft } from "react-icons/hi2";
+import Link from "next/link";
+import { Toaster } from "sonner";
 import ProductForm from "@/components/ProductForm";
 
 
@@ -10,9 +13,19 @@ const editPro = () => {
   return (
     <LayoutMain>
       <div className="flex flex-col bg-red-50 h-screen overflow-hidden">
-        <Toaster position="top-right" richColors />
-        <span className="p-3 text-2xl text-center font-bold shadow-sm bg-white">
-          Edit Product
+        <Toaster closeButton position="top-right" richColors />
+        <span className="p-3 flex justify-between shadow-sm bg-white">
+          <Link
+            href={"/products"}
+            className="text-sm flex justify-center items-center bg-blue-600 px-3 pl-2 py-1 group hover:bg-blue-700 rounded-full text-white"
+          >
+            <span className="flex gap-1 justify-center items-center">
+              <HiChevronLeft className="text-lg" />
+              <span>Back</span>
+            </span>
+          </Link>
+          <span className="text-2xl font-bold"> Edit Product</span>
+          <span></span>
         </span>
         <ProductForm />
       </div>
--- ecommerce-admin-panel/app/products/new/page.js
+++ ecommerce-admin-panel/app/products/new/page.js
@@ -1,20 +1,30 @@
+"use client";
 import LayoutMain from "@/components/MainLayout";
 import ProductForm from "@/components/ProductForm";
+import Link from "next/link";
 import React from "react";
+import { HiChevronLeft } from "react-icons/hi2";
 import { Toaster } from "sonner";
 
 const page = () => {
-
-
   return (
     <LayoutMain>
       <div className="flex flex-col bg-red-50 h-screen overflow-hidden">
-        <Toaster position="top-right" richColors />
-        <span className="p-3 text-2xl text-center font-bold shadow-sm bg-white">
-          New Product
+        <Toaster closeButton position="top-right" richColors />
+        <span className="p-3 flex justify-between shadow-sm bg-white">
+          <Link
+            href={"/products"}
+            className="text-sm flex justify-center items-center bg-blue-600 px-3 pl-2 py-1 group hover:bg-blue-700 rounded-full text-white"
+          >
+            <span className="flex gap-1 justify-center items-center">
+              <HiChevronLeft className="text-lg" />
+              <span>Back</span>
+            </span>
+          </Link>
+          <span className="text-2xl font-bold"> New Product</span>
+          <span></span>
         </span>
         <ProductForm />
-
       </div>
     </LayoutMain>
   );
