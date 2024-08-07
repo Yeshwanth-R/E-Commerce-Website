3e6768eb050c011ca8d76e474c4b155eb7f39e50
YESHWANTH R <eng23cs0234@dsu.edu.in>
2024-08-07 13:53:26 +0530

    Adding extra property to category done

--- ecommerce-admin-panel/models/categories.js
+++ ecommerce-admin-panel/models/categories.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true },
     parent: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    properties: [{ type: Object }]
 });
 
 const Category =
