687571159ef58f2e13ef9e212fddcc6d06d0d030
YESHWANTH R <eng23cs0234@dsu.edu.in>
2024-09-01 11:28:22 +0530

    Small changes

--- ecommerce-frontend/app/page.js
+++ ecommerce-frontend/app/page.js
@@ -1,7 +1,10 @@
 import Header from "@/components/Header";
 import HeroItem from "@/components/HeroItem";
+import connectDB from "@/lib/connectDB";
+import Products from "@/models/products";
 
-export default function Home() {
+export default function Home({ Products }) {
+  console.log(Products);
   return (
     <>
       <div>
@@ -10,4 +13,13 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export async function GetProducts() {
+  const HeroProductID = "66c6f85da567fc650f3de003";
+  await connectDB();
+  const FeaturedProduct = Products.findById(HeroProductID);
+  return {
+    props: FeaturedProduct,
+  };
 }
